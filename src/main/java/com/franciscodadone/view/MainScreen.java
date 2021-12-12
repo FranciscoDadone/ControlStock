3a997da1624e287589069a2b3ab4459208e42a1b
FranciscoDadone <dadonefran@gmail.com>
2021-12-12 11:54:10 -0300

    Session open when closing app behavior

--- src/main/java/com/franciscodadone/view/MainScreen.java
+++ src/main/java/com/franciscodadone/view/MainScreen.java
@@ -1,10 +1,16 @@
 package com.franciscodadone.view;
 
 import com.franciscodadone.controller.MainScreenController;
+import com.franciscodadone.model.local.queries.SessionsQueries;
+import com.franciscodadone.models.Session;
 import com.franciscodadone.util.GUIHandler;
+import com.franciscodadone.util.JCustomOptionPane;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.Date;
 
 public class MainScreen extends JFrame {
     private JPanel panel;
@@ -16,12 +22,28 @@ public class MainScreen extends JFrame {
         new MainScreenController(this);
 
         if(buildFrame) {
-            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             this.setContentPane(panel);
             this.pack();
             this.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
 //        this.setIconImage(new ImageIcon(getClass().getResource("/images/logo.jpg")).getImage());
+
+            this.addWindowListener(new WindowAdapter(){
+                public void windowClosing(WindowEvent e) {
+                    Session activeSession = SessionsQueries.getActiveSession();
+                    if(activeSession != null) {
+                        int res = JCustomOptionPane.confirmDialog("Advertencia, hay un turno abierto. ¿Desea cerrarlo antes de salir?", "Turno abierto");
+                        if(res == JOptionPane.YES_OPTION) {
+                            double endMoney = JCustomOptionPane.endSessionDialog();
+                            if(endMoney != -1) {
+                                SessionsQueries.endCurrentSession(endMoney);
+                                JCustomOptionPane.messageDialog("Turno gurdado exitosamente!", "", JOptionPane.PLAIN_MESSAGE);
+                                System.exit(0);
+                            }
+                        } else System.exit(0);
+                    } else System.exit(0);
+                }
+            });
 
             this.setVisible(true);
         }
